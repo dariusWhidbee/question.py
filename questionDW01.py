496cddf575179b6f7f9d1fb132d530118b18d95c
csPython <cspython@AD25TF04NGF1J-78.local>
2019-01-04 09:47:16 -0500

    second commit

--- questionDW01.py
+++ questionDW01.py
@@ -1,14 +1,30 @@
+# Add Variables, question and response
+
+# variable
+correct = 0
+qla = False 
+qlr = 3
+
+# question
+print("""What object has teeth but can't bite?
+
+1) A zipper
+2) A saw
+3) A comb
+4) A fork""")
+
 while qla == False:
     try:
         qlr = int(input("Choose the wisely. > "))
 
         if qlr == 3:
             correct = correct + 1
+            print("Exactly 3 is correct. Things like a saw, "
+                  "zipper and fork could 'bite' you")
             qla = True
         elif 0 < qlr < 5:
             qlr = True
         else:
             print("please choose an interger from 1 to 4")
-        except ValueError:
-            print("Invalid response.Please re-run the program and read"
-                  "the question carefully")
+    except ValueError:
+         print("Invalid response.")
